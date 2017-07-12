47c1e3941e6fab4d0b6df7f6b8d15c72120d696a
sarovin <sarovin86@gmail.com>
2017-07-12 15:34:23 +0200

    mv test to __test__ folder

--- __tests__/ScrollViewSmart.test.js
+++ __tests__/ScrollViewSmart.test.js
@@ -2,17 +2,14 @@
  * eslint-env jest
  */
 
-
 import 'react-native';
 import React from 'react';
-import ScrollViewSmart from './ScrollViewSmart';
+import ScrollViewSmart from '../lib/ScrollViewSmart';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <ScrollViewSmart />
-  ).toJSON();
+  const tree = renderer.create(<ScrollViewSmart />).toJSON();
   expect(tree).toMatchSnapshot();
 });
